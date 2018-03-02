905f4ab7f88a9bc81628b3773de33629db5d3828
pione30 <g.pionelish30@gmail.com>
2018-03-02 22:16:32 +0900

    on-off button

--- src/popup.js
+++ src/popup.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggle_switch = document.getElementById('darken_toggle')
 
   toggle_switch.addEventListener('change', () => {
+
+    // change popup.html colors
+    document.body.style.backgroundColor = toggle_switch.checked ? '#0a0a0a' : 'white'
+    document.body.style.color = toggle_switch.checked ? '#aaaaaa' : 'black'
+
+    // change colors of current tab
     chrome.tabs.executeScript(
       { code: 'var checked = ' + toggle_switch.checked.toString() + ';' },
       () => {
@@ -11,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         )
       }
     )
+
   })
 
 })
