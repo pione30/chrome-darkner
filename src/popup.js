5a27688258efcd1d10b4487b96aab7a9d55e2784
pione30 <g.pionelish30@gmail.com>
2018-03-14 22:31:17 +0900

    import allColorChange to popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,24 @@
+import allColorChange from './allColorChange.js'
+
 document.addEventListener('DOMContentLoaded', () => {
 
   const toggle_switch = document.getElementById('darken_toggle')
 
   toggle_switch.addEventListener('change', () => {
 
-    // change popup.html colors
-    document.body.style.backgroundColor = toggle_switch.checked ? '#0a0a0a' : 'white'
-    document.body.style.color = toggle_switch.checked ? '#aaaaaa' : 'black'
+    const checked = toggle_switch.checked
+
+    // change popup.html colors themselves
+    allColorChange(
+      {
+        backgroundColor: checked ? '#0a0a0a' : 'white',
+        color: checked ? '#aaaaaa' : 'black'
+      }
+    )
 
     // change colors of current tab
     chrome.tabs.executeScript(
-      { code: 'var checked = ' + toggle_switch.checked.toString() + ';' },
+      { code: 'var checked = ' + checked.toString() + ';' },
       () => {
         chrome.tabs.executeScript(
           { file: './js/checked.js' }
