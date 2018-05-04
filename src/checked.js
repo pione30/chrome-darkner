3eee0d3837ed053b46714707d3387d453d8585b6
pione30 <g.pionelish30@gmail.com>
2018-05-04 17:29:41 +0900

    add colorDefinition

--- src/checked.js
+++ src/checked.js
@@ -1,8 +1,10 @@
 import allColorChange from './allColorChange.js'
+import { color_definition } from './colorDefinition.js'
 
 allColorChange(
   {
-    backgroundColor: checked ? '#0a0a0a' : 'white',
-    color: checked ? '#aaaaaa' : 'black'
+    backgroundColor: checked ? color_definition['checked_backgroundColor'] : color_definition['not_checked_backgroundColor'],
+
+    color: checked ? color_definition['checked_color'] : color_definition['not_checked_color']
   }
 )
---
+++ src/colorDefinition.js
@@ -0,0 +1,9 @@
+export const color_definition = {
+
+  checked_backgroundColor:     '#0a0a0a',
+  not_checked_backgroundColor: 'white',
+
+  checked_color:               '#aaaaaa',
+  not_checked_color:           'black'
+
+}
