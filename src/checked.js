2147e5b28ed345fda4e1ee31af3bfdb1b6f25129
pione30 <g.pionelish30@gmail.com>
2018-11-11 22:55:48 +0900

    'checked' should be passed as a message

--- src/checked.js
+++ src/checked.js
@@ -1,8 +1,12 @@
 import searchAndColorChange from './searchAndColorChange.js'
 import colorDefinition from './colorDefinition.js'
 
-// To change the color of margin, the style in the 'html' tag
-// should be changed, while document.documentElement is
-// read-only property. So you should access 'html' tag
-// via document.body.parentNode.
-searchAndColorChange(document.body.parentNode, colorDefinition(checked))
+chrome.runtime.onMessage.addListener(
+  function(request) {
+    // To change the color of margin, the style in the 'html' tag
+    // should be changed, while document.documentElement is
+    // read-only property. So you should access 'html' tag
+    // via document.body.parentNode.
+    searchAndColorChange(document.body.parentNode, colorDefinition(request.checked))
+  }
+)
--- src/popup.js
+++ src/popup.js
@@ -6,6 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const toggle_switch = document.getElementById('darken_toggle')
 
+  // inject content scripts which recieves a message
+  chrome.tabs.executeScript(
+    { file: './js/checked.js' }
+  )
+
   toggle_switch.addEventListener('change', () => {
 
     const checked = toggle_switch.checked
@@ -14,14 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
     searchAndColorChange(document.body.parentNode, colorDefinition(checked))
 
     // change colors of current tab
-    chrome.tabs.executeScript(
-      { code: 'var checked = ' + checked.toString() + ';' },
-      () => {
-        chrome.tabs.executeScript(
-          { file: './js/checked.js' }
-        )
-      }
-    )
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { checked: checked })
+    })
 
   })
 
