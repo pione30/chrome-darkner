e9d60f174a0479dd7ac2cf5352f5a02045dae513
pione30 <g.pionelish30@gmail.com>
2018-11-13 00:25:57 +0900

    no need of babel since this is _chrome_ extension

--- webpack.common.js
+++ webpack.common.js
@@ -8,16 +8,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
         test: /\.css$/,
         use: [
           'style-loader',
