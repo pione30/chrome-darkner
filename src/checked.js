4f954a6106f11a246603ba48477a6cc29facfb2f
pione30 <g.pionelish30@gmail.com>
2018-11-12 21:49:09 +0900

    checked.js recieves excludedNodes through message

--- src/checked.js
+++ src/checked.js
@@ -1,9 +1,8 @@
 import searchAndColorChange from './searchAndColorChange.js'
 import colorDefinition from './colorDefinition.js'
-import excludeNodes from './excludeNodes.js'
 
 chrome.runtime.onMessage.addListener(
   (request) => {
-    searchAndColorChange(document.documentElement, colorDefinition(request.checked), excludeNodes)
+    searchAndColorChange(document.documentElement, colorDefinition(request.checked), request.excludedNodes)
   }
 )
