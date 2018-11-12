dc4d07458f888ce113edbe02376ebeec1e1e1ebf
pione30 <g.pionelish30@gmail.com>
2018-11-12 22:59:18 +0900

    :bang:

--- src/popup.js
+++ src/popup.js
@@ -5,22 +5,34 @@ import './style.css'
 document.addEventListener('DOMContentLoaded', () => {
 
   const toggle_switch = document.getElementById('darken_toggle')
+  const includedNodes = document.querySelectorAll('#includedNodes label input')
+  const container = document.getElementById('container')
 
   // inject content scripts which recieves a message
   chrome.tabs.executeScript(
     { file: './js/checked.js' }
   )
 
-  toggle_switch.addEventListener('change', () => {
+  container.addEventListener('change', () => {
 
     const checked = toggle_switch.checked
 
+    let excludedNodes = []
+    for(const includeNode of includedNodes) {
+      if(!includeNode.checked) {
+        excludedNodes.push(includeNode.value)
+      }
+    }
+
     // change popup.html colors themselves
-    searchAndColorChange(document.documentElement, colorDefinition(checked))
+    searchAndColorChange(document.documentElement, colorDefinition(checked), excludedNodes)
 
     // change colors of current tab
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { checked: checked })
+      chrome.tabs.sendMessage(tabs[0].id, {
+        checked: checked,
+        excludedNodes: excludedNodes
+      })
     })
 
   })
